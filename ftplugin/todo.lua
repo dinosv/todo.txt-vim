3aa325b53fd028beb7b588776f505116f7c80812
Dino Sepúlveda <dinosepulvedav@gmail.com>
2026-01-16 15:41:13 -0300

    feat(ftplugin): add hidden task highlighting

--- ftplugin/todo.lua
+++ ftplugin/todo.lua
@@ -22,3 +22,34 @@ vim.keymap.set("n", "<localleader>x", function()
     vim.api.nvim_buf_set_lines(0, lnum, lnum, false, { new_task })
   end
 end, { buffer = true, desc = "Mark todo as done" })
+
+-- Define highlight group
+vim.api.nvim_set_hl(0, "TodoHidden", { link = "Comment", default = true })
+vim.api.nvim_set_hl(0, "TodoRecurring", { link = "Special", default = true })
+
+-- Namespace for our highlights
+local ns = vim.api.nvim_create_namespace("todotxt")
+
+local function update_highlights()
+  if not todotxt.config.threshold_highlight then
+    return
+  end
+
+  vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  for i, line in ipairs(lines) do
+    if threshold.is_hidden(line) then
+      vim.api.nvim_buf_add_highlight(0, ns, "TodoHidden", i - 1, 0, -1)
+    end
+  end
+end
+
+-- Update on buffer changes
+vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI" }, {
+  buffer = 0,
+  callback = update_highlights,
+})
+
+-- Initial highlight
+update_highlights()
